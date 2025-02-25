0b1e54b8a01111840462f7396d4f1b921399d2e8
pearl <pelroma22@src.pshs.edu.ph>
2025-02-25 20:38:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function checkMessage() {
+	message = prompt('What\'s the word?');
+	
+	if(message.includes('FR')) {
+		document.getElementById('result').innerHTML = 'The message ' + '"' + message + '"' + ' is legitimate!';
+	}
+	else if(message.includes('FR') && message.includes('AI')) {
+		document.getElementById('result').innerHTML = 'The message ' + '"' + message + '"' + ' is legitimate!';
+	}
+	else if(message.includes('AI')) {
+		document.getElementById('result').innerHTML = 'The message ' + '"' + message + '"' + ' is fake!';
+	}
+	else if(message.includes('aI') || message.includes('Ai')) {
+		document.getElementById('result').innerHTML = 'The message ' + '"' + message + '"' +  ' is fake!';
+	}
+	else {
+		document.getElementById('result').innerHTML = 'The message ' + '"' + message + '"' + ' is not yet encoded!';
+	}
+}
