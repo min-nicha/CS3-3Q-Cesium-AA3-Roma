3f3938b7237b20f3d443265122f4f2347c2474c3
pearl <pelroma22@src.pshs.edu.ph>
2025-03-03 18:38:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function checkMessage() {
 	message = prompt('What\'s the word?');
 	
-	if(message.includes('FR')) {
+	if(message.includes('FR') && message.includes('AI')) {
 		document.getElementById('result').innerHTML = 'The message ' + '"' + message + '"' + ' is legitimate!';
 	}
-	else if(message.includes('FR') && message.includes('AI')) {
-		document.getElementById('result').innerHTML = 'The message ' + '"' + message + '"' + ' is legitimate!';
+	else if(message.includes('aI') || message.includes('Ai')) {
+		document.getElementById('result').innerHTML = 'The message ' + '"' + message + '"' + ' is fake!';
 	}
 	else if(message.includes('AI')) {
 		document.getElementById('result').innerHTML = 'The message ' + '"' + message + '"' + ' is fake!';
 	}
-	else if(message.includes('aI') || message.includes('Ai')) {
-		document.getElementById('result').innerHTML = 'The message ' + '"' + message + '"' +  ' is fake!';
+	else if(message.includes('FR')) {
+		document.getElementById('result').innerHTML = 'The message ' + '"' + message + '"' +  ' is legitimate!';
 	}
 	else {
 		document.getElementById('result').innerHTML = 'The message ' + '"' + message + '"' + ' is not yet encoded!';
